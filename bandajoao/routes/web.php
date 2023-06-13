e94b7d9c9143bd4f62697eba00195c62c7e5e895
João Mendes <104670865+Mendes2099@users.noreply.github.com>
2023-06-13 19:48:46 +0100

    Update nas rotas

--- bandajoao/routes/web.php
+++ bandajoao/routes/web.php
@@ -1,10 +1,14 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BandaController;
 use App\Http\Controllers\AlbumController;
 use App\Http\Controllers\UserController;
 
+use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
+use Laravel\Fortify\Http\Controllers\RegisteredUserController;
+use Laravel\Fortify\Http\Controllers\PasswordResetLinkController;
+use Laravel\Fortify\Http\Controllers\NewPasswordController;
+use Laravel\Fortify\Http\Controllers\PasswordResetController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,7 +21,6 @@ use App\Http\Controllers\UserController;
 */
 
 Route::get('/', [UserController::class, 'index'])->name('home');
-
 
 //!---------------------------------------------------------------------------------------------
 
@@ -50,4 +53,38 @@ Route::put('/bandas/{banda}/albuns/{album}', [AlbumController::class, 'update'])
 
 // Rota para excluir álbuns (apenas para administradores)
 Route::delete('/bandas/{banda}/albuns/{album}', [AlbumController::class, 'destroy'])->name('albuns.destroy')->middleware('admin');
+
+//!---------------------------------------------------------------------------------------------
+
+// Rotas do Fortify
+Route::prefix('user')->group(function () {
+    Route::middleware('guest')->group(function () {
+        Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
+        Route::post('/register', [RegisteredUserController::class, 'store']);
+
+        Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
+        Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+
+        Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
+        Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
+
+        Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
+        Route::post('/reset-password', [NewPasswordController::class, 'store'])->name('password.update');
+    });
+
+    Route::middleware('auth')->group(function () {
+        Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+    });
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
+    Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+});
+
 
