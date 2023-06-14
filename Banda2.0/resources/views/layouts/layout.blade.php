2d18af6c10ceef3bc4bcf5c76eeec379a827fad4
João Mendes <104670865+Mendes2099@users.noreply.github.com>
2023-06-14 06:59:37 +0100

    Alterações no layout

--- Banda2.0/resources/views/layouts/layout.blade.php
+++ Banda2.0/resources/views/layouts/layout.blade.php
@@ -4,7 +4,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bandas do Johnny</title>
-    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <style>
+        /*CSS*/
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+        header {
+            background-color: #333;
+            color: #fff;
+            padding: 10px;
+        }
+        nav ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+        }
+        nav ul li {
+            display: inline-block;
+            margin-right: 10px;
+        }
+        nav ul li a {
+            color: #fff;
+            text-decoration: none;
+            padding: 5px;
+        }
+        main {
+            padding: 20px;
+        }
+        footer {
+            background-color: #f5f5f5;
+            padding: 10px;
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
     <header>
@@ -19,7 +54,7 @@
     </header>
 
     <main>
-        {{ $slot }}
+        @yield('content')
     </main>
 
     <footer>
